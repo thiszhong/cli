870750efc19b244c5fece30e6eb0388b89b62c89
zhong <384232149@qq.com>
2021-02-20 18:34:29 +0800

    add

---
+++ src/pages/About.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  async setup() {
+    return {
+      msg: 'About'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
